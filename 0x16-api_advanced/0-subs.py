3f2b94dfa4054e23b7afe778b2e55d4df5b98fd9
dan24-T <106978779+dan24-T@users.noreply.github.com>
2023-05-12 00:43:46 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,1 @@
-#!/usr/bin/python3
-"""
-number_of_subscribers task - api Adavanced
-"""
-
-import requests
-
-
-def number_of_subscribers(subreddit):
-    """Query that returns number of subscribers of a subreddit"""
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-    req = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': 'My-User-Agent'},allow_redirects=False).json()
-    subs = req.get("data", {}).get("subscribers", 0)
-    return subs
+""""Subs"""
