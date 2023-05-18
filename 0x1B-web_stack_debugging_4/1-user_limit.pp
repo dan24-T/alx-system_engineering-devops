6122ecbb3c64310eac01c6b9b4dec957e0c79e46
dan24-T <106978779+dan24-T@users.noreply.github.com>
2023-05-19 01:41:25 +0300

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,1 +1,13 @@
-#also kind of similar on above snippett
+# also kind of similar on above snippett
+
+# increase the hard-file-limit
+exec { 'increase-hard-file-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase the soft file limit.
+exec { 'increase-soft-file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
